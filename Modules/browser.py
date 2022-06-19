996ba98daa427c425f6daebd3308eae801f60b7b
delta <delta6862@protonmail.com>
2022-06-19 20:20:17 +0200

    Added CORN and EXSS chall infra
    Signed-off-by: delta <delta6862@protonmail.com>

--- Modules/browser.py
+++ Modules/browser.py
@@ -14,54 +14,75 @@ import discord
 import time
 import re
 
+try:
+    from . import ctf
 
-class Browser:
-    def __init__(self, client):
-        self.commands = {
-            "visit": self.visit,
-        }
-        self.name = ["Browser"]
-        self.category = ["CTF"]
-        self.client = client
-        self.type = "Runtime"
-        self.browser = None
-        self.challenges = {}
-        self.users = {}
+    class Browser:
+        def __init__(self, client):
+            self.commands = {
+                "visit": self.visit,
+            }
+            self.name = ["Browser"]
+            self.category = ["CTF"]
+            self.client = client
+            self.type = "Runtime"
+            self.browser = None
+            self.challenges = {
+                "corn": self.corn,
+                "exss": self.exss
+            }
+            self.users = {}
 
-    async def process_message(self, message):
-        if not isinstance(message.channel, discord.channel.DMChannel):  # Module only works in DMs
-            return
-        if message.content.split(" ")[0][1:] in self.commands.keys():
-            return self.commands[message.content.split(" ")[0][1:]](message)
+        async def process_message(self, message):
+            if not isinstance(message.channel, discord.channel.DMChannel):  # Module only works in DMs
+                return
+            if message.content.split(" ")[0][1:] in self.commands.keys():
+                return self.commands[message.content.split(" ")[0][1:]](message)
 
-    # This function tests the basic message sending functionality.
-    def visit(self, msg):
-        if len(msg.content.split(" ")) < 2:
-            return "Usage: !visit <url>"
-        url = msg.content.split(" ")[1]
-        if not re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@"
-                        r":%_\+.~#?&//=]*)", url):
-            return "Invalid URL"
+        # This function tests the basic message sending functionality.
+        def visit(self, msg):
+            if len(msg.content.split(" ")) < 2:
+                return "Usage: !visit <url>"
+            url = msg.content.split(" ")[1]
+            if not re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@"
+                            r":%_\+.~#?&//=]*)", url):
+                return "Invalid URL"
 
-        if msg.author in self.users.keys() and time.time - self.users[msg.author] < 5:
-            return "I can only visit one link every 5 seconds."
-        self.users[msg.author] = time.time()
+            if msg.author in self.users.keys() and time.time - self.users[msg.author] < 5:
+                return "I can only visit one link every 5 seconds."
+            self.users[msg.author] = time.time()
 
-        self.setup_browser()
-        self.browser.get(url)
+            self.setup_browser()
+            self.browser.get(url)
 
-        return "Visiting link..."
+            return "Visiting link..."
 
-    def setup_browser(self):
-        opts = Options()
-        opts.set_headless()
-        assert opts.headless
-        self.browser = webdriver.Firefox(options=opts)
-        self.browser.set_page_load_timeout(10)
+        def setup_browser(self):
+            opts = Options()
+            opts.set_headless()
+            assert opts.headless
+            self.browser = webdriver.Firefox(options=opts)
+            self.browser.set_page_load_timeout(10)
 
-        # Initialize the individual challenges
-        for i in self.challenges.keys:
-            i()
+            # Initialize the individual challenges
+            for i in self.challenges.keys:
+                self.challenges[i]()
+
+        def corn(self):
+            self.browser.get("http://146.190.16.124:/login")
+            username = self.browser.find_element_by_id("username")
+            password = self.browser.find_element_by_id("password")
+            username.send_keys("admin")
+            password.send_keys(ctf.corn["password"])
+            self.browser.find_element_by_name("login").click()
+
+        def exss(self):
+            self.browser.get("http://146.190.16.124:5080/")
+            cookie = self.browser.find_element_by_id("new-cookie")
+            cookie.send_keys(ctf.exss["flag"])
+            self.browser.find_element_by_id("add-cookie").click()
 
 
-registry.register(Browser)
+    registry.register(Browser)
+except ImportError:
+    pass
